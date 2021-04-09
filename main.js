28a3ee3d541ea9f730e584d614af3d6e6937da16
Mason <masonbush751@gmail.com>
2021-04-09 17:16:08 -0500

    Create city/ Retrieve all cities owned by user and display them

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const URL = 'http://localhost:3001/'
 const createAccount = document.querySelector('.createAccount')
 const loginAccount = document.querySelector('.loginToAccount')
 const createAccountBtn = document.querySelector('#createAccountBtn')
@@ -45,7 +46,7 @@ document.querySelector('#loginForm').addEventListener('submit', e => {
     e.preventDefault()
     const username = document.querySelector('#usernameFieldLogin')
     const password = document.querySelector('#passwordFieldLogin')
-    axios.post('http://localhost:3001/user/login', {
+    axios.post(`${URL}user/login`, {
         username: username.value,
         password: password.value
     }).then(res => {
@@ -69,10 +70,45 @@ document.querySelector('#createAccountForm').addEventListener('submit', e => {
     e.preventDefault()
     const username = document.querySelector('#usernameFieldCa')
     const password = document.querySelector('#passwordFieldCa')
-    axios.post('http://localhost:3001/user', {
+    axios.post(`${URL}user`, {
         username: username.value,
         password: password.value
     })
     username.value = null
     password.value = null
 })
+
+document.querySelector('#createCityForm').addEventListener('submit', e => {
+    e.preventDefault()
+    const cityName = document.querySelector('.createCityName')
+    const userId = localStorage.getItem('userIn')
+    axios.post(`${URL}city`, {
+        name: cityName.value,
+        infantryInCity: 0,
+        userId: userId
+    }).then(res => {
+        displayAllUserCities()
+    })
+    document.querySelector('.profileCreateCityPopup').classList.add('hidden')
+})
+
+const displayAllUserCities = async () => {
+    const cities = await axios.get(`${URL}city/${localStorage.getItem('userIn')}`)
+    const cityPath = cities.data.city
+    for(let i in cityPath) {
+        const newDiv = document.createElement('div')
+        const cityTitle = document.createElement('h2')
+        const cityTroops = document.createElement('p')
+
+        newDiv.classList.add('city')
+        cityTitle.classList.add('cityTitle')
+        cityTroops.classList.add('cityTroops')
+
+        cityTitle.innerText = cityPath[i].name
+        cityTroops.innerText = `Stationed Troops: ${cityPath[i].infantryInCity}`
+
+        newDiv.appendChild(cityTitle)
+        newDiv.appendChild(cityTroops)
+        document.querySelector('.citiesOwned').appendChild(newDiv)
+    }
+}
