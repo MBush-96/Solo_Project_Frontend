dd5d33f13196cd9462edc88ea3a41034117fcaa2
Mason <masonbush751@gmail.com>
2021-04-12 18:01:32 -0500

    frontend

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/main.js', async (req, res) => {
     await replaceInFile({
       files: filepath,
       from: 'http://localhost:3001/',
-      to: 'https://wargame-backend.herokuapp.com' 
+      to: 'https://wargame-backend.herokuapp.com/' 
     })
   }
 
